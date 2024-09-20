c88663245c2a31dd85c0cfc45dbfcb6f1a16187a
Alejandro Rodríguez Vallín <162721455+alejandroRV05@users.noreply.github.com>
2024-09-20 17:55:55 +0200

    Modificar Test

--- test.php
+++ test.php
@@ -3,7 +3,11 @@
     <body>
         <h1>Mi primer fichero en PHP</h1>
         <?php
+<<<<<<< Updated upstream
             echo '<p>Hola mundo</p>';
+=======
+            echo '<p>Hola mundo cruel loooooooooooooool</p>';
+>>>>>>> Stashed changes
             echo '<br>';
             echo '<p>Mi primer programa en PHP</p>';
             echo 'test para github';
