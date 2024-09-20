eecb3031cfc7ed9a6540ab91adc175b951b7d453
Alejandro Rodríguez Vallín <162721455+alejandroRV05@users.noreply.github.com>
2024-09-20 17:44:09 +0200

    Create test.php

---
+++ test.php
@@ -0,0 +1,12 @@
+<html>
+    <head></head>
+    <body>
+        <h1>Mi primer fichero en PHP</h1>
+        <?php
+            echo '<p>Hola mundo</p>';
+            echo '<br>';
+            echo '<p>Mi primer programa en PHP</p>';
+        ?> 
+    </body>
+</html>
+
