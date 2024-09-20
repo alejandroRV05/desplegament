62a126fb2d91a039c56bc75a6da4f93fdd963251
AndreuZapaterDev <andreuzapaterdev@gmail.com>
2024-09-20 17:50:25 +0200

    Update test.php
    Modificación párafo

--- test.php
+++ test.php
@@ -3,7 +3,7 @@
     <body>
         <h1>Mi primer fichero en PHP</h1>
         <?php
-            echo '<p>Hola mundo</p>';
+            echo '<p>Hola mundo cruel</p>';
             echo '<br>';
             echo '<p>Mi primer programa en PHP</p>';
         ?> 
